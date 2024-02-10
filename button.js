885b6558a18a99af46dfb07f6df3d4bebb4d7c39
Kent-Ugawa <141267792+Kent-Ugawa@users.noreply.github.com>
2024-02-10 14:59:11 +0900

    Delete button.js

--- button.js
+++
@@ -1,8 +0,0 @@
-const para = document.querySelector("p");
-
-para.addEventListener("click", updateName);
-
-function updateName() {
-    const name = prompt("名前を入力してください");
-    para.textContent = `Player 1: ${name}`;
-}
